4c791de50e838f308e2e1206a8ea79bc527fd465
Toni Petrina <toni.petrina@gmail.com>
2012-11-29 22:25:47 +0100

    Added the basis for XamlDirectXHelper for Win8

---
+++ XamlDirectXHelper/XamlDirectXHelper/SurfaceImageSourceWrapper.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+namespace XamlDirectXHelper
+{
+    public ref class SurfaceImageSourceWrapper sealed : Windows::UI::Xaml::Media::Imaging::SurfaceImageSource
+	{
+	private protected:
+		Microsoft::WRL::ComPtr<ISurfaceImageSourceNative>	_sisNative;
+
+		Microsoft::WRL::ComPtr<ID3D11Device>				_d3dDevice;
+		Microsoft::WRL::ComPtr<ID3D11DeviceContext>			_d3dContext;
+		Microsoft::WRL::ComPtr<IDXGIDevice>					_dxgiDevice;
+
+		Microsoft::WRL::ComPtr<ID2D1Device>					_d2dDevice;
+		Microsoft::WRL::ComPtr<ID2D1DeviceContext>			_d2dDeviceContext;
+
+		int		_height;
+		int		_width;
+		float	_dpi;
+
+		void CreateDeviceIndependentResources();
+		void CreateDeviceResources();
+
+	public:
+		SurfaceImageSourceWrapper(int pixelWidth, int pixelHeight);
+		SurfaceImageSourceWrapper(int pixelWidth, int pixelHeight, bool isOpaque);
+
+		void BeginDraw(Windows::Foundation::Rect updateRect);
+		void BeginDraw()
+		{
+			BeginDraw(Windows::Foundation::Rect(0, 0, (float)_width, (float)_height));
+		}
+		void EndDraw();
+
+		void SetDpi(float dpi);
+
+		int GetDeviceContext() { return ((Platform::IntPtr)_d2dDeviceContext.Get()).ToInt32(); }
+	};
+}
---
+++ XamlDirectXHelper/XamlDirectXHelper/pch.h
@@ -0,0 +1,27 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include <D3D11.h>
+#include <d2d1_1.h>
+
+#include <wrl.h>
+#include <windows.ui.xaml.media.dxinterop.h>
+
+
+// Helper utilities to make DX APIs work with exceptions in the samples apps.
+namespace DX
+{
+    inline void ThrowIfFailed(HRESULT hr)
+    {
+        if (FAILED(hr))
+        {
+            // Set a breakpoint on this line to catch DX API errors.
+            throw Platform::Exception::CreateException(hr);
+        }
+    }
+}
+
