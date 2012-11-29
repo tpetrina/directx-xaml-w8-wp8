0c5314bd4bb61f4de47f8eba581b45ffd969e9bc
Toni Petrina <toni.petrina@gmail.com>
2012-11-29 22:25:21 +0100

    Added sample code for the blog post about buggy...
    ...default project template for Windows Phone 8 with both Direct3D and
    XAML using C#.

---
+++ MiscProjects/PhoneXamlDirect3DAppNavigationProblem/PhoneXamlDirect3DAppNavigationProblem/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneXamlDirect3DAppNavigationProblemComp;
+
+namespace PhoneXamlDirect3DAppNavigationProblem
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private Direct3DInterop m_d3dInterop = new Direct3DInterop();
+        private object m_d3dContentProvider = null;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawingSurface_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Set window bounds in dips
+            m_d3dInterop.WindowBounds = new Windows.Foundation.Size(
+                (float)DrawingSurface.ActualWidth,
+                (float)DrawingSurface.ActualHeight
+                );
+
+            // Set native resolution in pixels
+            m_d3dInterop.NativeResolution = new Windows.Foundation.Size(
+                (float)Math.Floor(DrawingSurface.ActualWidth * Application.Current.Host.Content.ScaleFactor / 100.0f + 0.5f),
+                (float)Math.Floor(DrawingSurface.ActualHeight * Application.Current.Host.Content.ScaleFactor / 100.0f + 0.5f)
+                );
+
+            // Set render resolution to the full native resolution
+            m_d3dInterop.RenderResolution = m_d3dInterop.NativeResolution;
+
+            // Hook-up native component to DrawingSurface
+            if (m_d3dContentProvider == null)
+                m_d3dContentProvider = m_d3dInterop.CreateContentProvider();
+            DrawingSurface.SetContentProvider(m_d3dContentProvider);
+            DrawingSurface.SetManipulationHandler(m_d3dInterop);
+        }
+
+        private void _btnHighScores_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/HighScoresPage.xaml", UriKind.Relative));
+        }
+    }
+}
